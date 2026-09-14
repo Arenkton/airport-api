c52b6fe4e7ab11afdcd52561b7b541643f5dcae3
Artem Yur <artemonodon@gmail.com>
2026-09-14 14:57:35 +0200

    Implement airport and rout models

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Airport(models.Model):
+    name = models.CharField(max_length=255)
+    closest_big_city = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.closest_big_city})"
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="routes_from",
+    )
+    destination = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="routes_to",
+    )
+    distance = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.source.name} - {self.destination.name}"
+
+
