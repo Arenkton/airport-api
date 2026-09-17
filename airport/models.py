f2bbcabb94c8d561f75b30268741d04f5a403e45
Artem Yur <artemonodon@gmail.com>
2026-09-17 12:31:28 +0200

    Implement crew and flight models

--- airport/models.py
+++ airport/models.py
@@ -45,3 +45,32 @@ class Airplane(models.Model):
     def __str__(self):
         return self.name
 
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Flight(models.Model):
+    route = models.ForeignKey(
+        Route,
+        on_delete=models.CASCADE,
+        related_name="flights",
+    )
+    airplane = models.ForeignKey(
+        Airplane,
+        on_delete=models.CASCADE,
+        related_name="flights",
+    )
+    crew = models.ManyToManyField(
+        Crew,
+        related_name="flights",
+    )
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.route} | {self.departure_time}"
