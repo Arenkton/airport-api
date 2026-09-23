01177fb07132250a2f3b164f215aa1d5bf469693
Artem Yur <artemonodon@gmail.com>
2026-09-23 10:57:40 +0200

    Add Swagger API documentation

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'drf_spectacular',
     'airport',
     'user',
 ]
@@ -135,4 +136,17 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ],
+    "DEFAULT_SCHEMA_CLASS": (
+        "drf_spectacular.openapi.AutoSchema"
+    ),
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Airport API",
+    "DESCRIPTION": (
+        "REST API for airport management, "
+        "flight scheduling and ticket booking."
+    ),
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
 }
